fbeadaef8f8fd918cab7c115fcd912e9a473752e
Philippe Proulx <eeppeliteloop@gmail.com>
2025-01-30 15:40:47 -0500

    src/common/align.h: remove unused macros
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I4e27b0011997e9e8638f88a1291b557d64d5ef88
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/14002

--- src/common/align.h
+++ src/common/align.h
@@ -12,11 +12,7 @@
 
 #define BT_ALIGN(x, a)		__BT_ALIGN_MASK(x, (__typeof__(x))(a) - 1)
 #define __BT_ALIGN_MASK(x, mask)	(((x) + (mask)) & ~(mask))
-#define BT_PTR_ALIGN(p, a)		((__typeof__(p)) BT_ALIGN((unsigned long) (p), a))
 #define BT_ALIGN_FLOOR(x, a)	__BT_ALIGN_FLOOR_MASK(x, (__typeof__(x)) (a) - 1)
 #define __BT_ALIGN_FLOOR_MASK(x, mask)	((x) & ~(mask))
-#define BT_PTR_ALIGN_FLOOR(p, a) \
-			((__typeof__(p)) BT_ALIGN_FLOOR((unsigned long) (p), a))
-#define BT_IS_ALIGNED(x, a)	(((x) & ((__typeof__(x)) (a) - 1)) == 0)
 
 #endif /* BABELTRACE_COMMON_ALIGN_H */
