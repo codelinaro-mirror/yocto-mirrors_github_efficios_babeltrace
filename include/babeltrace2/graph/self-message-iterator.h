argonne-lcf/babeltrace

doc: libbabeltrace2: "You can only" 👉 "You may only"
Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
Change-Id: I68a84de120b15126e7dfa1da6b1033a9ac9b20ca

include/babeltrace2/graph/self-message-iterator.h
@@ -206,7 +206,7 @@ A message iterator can seek forward if all the \bt_p_msg of its
 message sequence have some \bt_cs.
 
 @attention
-    You can only call this function during the execution of the
+    You may only call this function during the execution of the
     \ref api-msg-iter-cls-meth-init "initialization method" of a
     message iterator.
 

codelinaro-mirror/yocto-mirrors_github_efficios_babeltrace

Change-Id: Ie734ae521390362b51082cd0a4bbabd7a781961a

include/babeltrace2/graph/self-message-iterator.h
@@ -217,7 +217,7 @@ A message iterator can seek forward if all the \bt_p_msg of its
 message sequence have some \bt_cs.
 
 @attention
-    You can only call this function during the execution of a
+    You may only call this function during the execution of a
     message iterator's
     \ref api-msg-iter-cls-meth-init "initialization method".
 