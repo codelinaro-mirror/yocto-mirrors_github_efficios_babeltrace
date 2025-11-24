463b6f79589e7fc391e2a2a669723f792e06dd88
Philippe Proulx <eeppeliteloop@gmail.com>
2025-11-24 13:28:14 -0500

    tests: bt_tests_utils.py: add pytest shims to support pytest 6+
    This patch adds new classes and a function to
    `tests/utils/python/bt_tests_utils.py` to support pytest 6 using the
    pytest 7+ API:
    
    `PytestItem`:
        A `pytest.Item` which offers the pytest 7+ API even when working
        with pytest 6:
    
        • path() is always available (returns `pathlib.Path`).
    
    `PytestFile`:
        A `pytest.File` which offers the pytest 7+ API even when working
        with pytest 6:
    
        • from_parent() accepts `path` (`pathlib.Path` type).
        • path() is always available (returns `pathlib.Path`).
    
        Within your own collect(), create items of which the class
        inherits `PytestItem`.
    
    install_pytest_collect_file():
        Installs a pytest_collect_file() hook within the given module which
        calls a function which expects the pytest_collect_file() API of
        pytest 7+.
    
        This is needed because pytest checks the parameter names of hooks,
        and the `path` parameter (old `py.path.local` type) is deprecated in
        pytest 7+ in favor of `file_path` (`pathlib.Path` type).
    
        Use like this:
    
            def _my_pytest_collect_file(file_path: pathlib.Path, parent):
                # ...
    
            btu.install_pytest_collect_file(sys.modules[__name__],
                                            _my_pytest_collect_file)
    
        Within your function, create a file object which
        inherits `PytestFile`.
    
    Therefore, if you want to collect custom files in some `conftest.py`:
    
    1. Use `btu.PytestItem` and `btu.PytestFile` instead of `pytest.Item`
       and `pytest.File`.
    
       Use the pytest 7+ API, using `path` instead of `fspath`.
    
    2. Create a private function acting as your pytest_collect_file() hook,
       for example _pytest_collect_file().
    
       Use the pytest 7+ API, using `file_path` instead of `path`.
    
    3. Install the real pytest_collect_file() hook to call back yours:
    
           btu.install_pytest_collect_file(sys.modules[__name__],
                                           _pytest_collect_file)
    
    Then your `conftest.py` works with pytest 6 and pytest 7+ without errors
    or deprecation warnings.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I5b43ba1fad31b171a8078833eb404777a64afd20

--- tests/utils/python/bt_tests_utils.py
+++ tests/utils/python/bt_tests_utils.py
@@ -4,8 +4,15 @@
 # pyright: strict
 
 import enum
+import types
+import logging
 import pathlib
 import platform
+from typing import Any, Callable, Optional
+
+import pytest
+
+_logger = logging.getLogger(__name__)
 
 
 class OsType(enum.Enum):
@@ -46,3 +53,130 @@ def os_type() -> OsType:
 # `tests` directory).
 def bt2_package_dir(build_root_dir: pathlib.Path) -> pathlib.Path:
     return build_root_dir / "src/bindings/python/bt2/build/build_lib"
+
+
+# A `pytest.Item` which offers the pytest 7+ API even when working
+# with pytest 6:
+#
+# • path() is always available (returns `pathlib.Path`).
+class PytestItem(pytest.Item):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)  # pyright: ignore[reportUnknownMemberType]
+        assert "parent" in kwargs
+
+        if hasattr(kwargs["parent"], "fspath"):
+            # pytest 6
+            self._path = pathlib.Path(str(self.fspath))
+        else:
+            # pytest 7+
+            self._path = self.path
+
+    @property
+    def path(  # pyright: ignore[reportIncompatibleVariableOverride]
+        self,
+    ) -> pathlib.Path:
+        return self._path
+
+    @path.setter
+    def path(  # pyright: ignore[reportIncompatibleVariableOverride]
+        self, path: pathlib.Path
+    ) -> None:
+        self._path = path
+
+
+# A `pytest.File` which offers the pytest 7+ API even when working
+# with pytest 6:
+#
+# • from_parent() accepts `path` (`pathlib.Path` type).
+# • path() is always available (returns `pathlib.Path`).
+#
+# Within your own collect(), create items of which the class
+# inherits `PytestItem`.
+class PytestFile(pytest.File):
+    @classmethod
+    def from_parent(  # pyright: ignore[reportIncompatibleMethodOverride]
+        cls, parent: "pytest.Collector", *, path: pathlib.Path, **kwargs: Any
+    ) -> "PytestFile":
+        # Try pytest 7+ API first
+        try:
+            return super().from_parent(parent=parent, path=path, **kwargs)  # type: ignore[return-value]
+        except TypeError:
+            # Fall back to pytest 6 API
+            import py.path  # pyright: ignore[reportMissingModuleSource]
+
+            return super().from_parent(  # type: ignore[return-value]
+                parent=parent, fspath=py.path.local(str(path)), **kwargs
+            )
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        assert "path" in kwargs or "fspath" in kwargs
+
+        # Normalize the path
+        if "path" in kwargs:
+            # pytest 7+
+            self._path = self.path
+        elif "fspath" in kwargs:
+            # pytest 6
+            self._path = pathlib.Path(str(self.fspath))
+
+    @property
+    def path(
+        self,
+    ) -> pathlib.Path:  # pyright: ignore[reportIncompatibleVariableOverride]
+        return self._path
+
+    @path.setter
+    def path(
+        self, path: pathlib.Path
+    ) -> None:  # pyright: ignore[reportIncompatibleVariableOverride]
+        self._path = path
+
+
+# Installs a pytest_collect_file() hook within the module `module` which
+# calls `func` which expects the pytest_collect_file() API of pytest 7+.
+#
+# This is needed because pytest checks the parameter names of hooks, and
+# the `path` parameter (old `py.path.local` type) is deprecated in
+# pytest 7+ in favor of `file_path` (`pathlib.Path` type).
+#
+# Use like this:
+#
+#     def _my_pytest_collect_file(file_path: pathlib.Path, parent):
+#         # ...
+#
+#     btu.install_pytest_collect_file(sys.modules[__name__],
+#                                     _my_pytest_collect_file)
+#
+# Within `func`, create a file object which inherits `PytestFile`.
+def install_pytest_collect_file(
+    module: types.ModuleType,
+    func: Callable[..., Optional[PytestFile]],
+) -> None:
+    if hasattr(pytest, "version_tuple"):
+        # pytest 7+: expect a `pathlib.Path` path
+        def pytest_7_hook(
+            file_path: pathlib.Path, parent: "pytest.Collector"
+        ) -> Optional[PytestFile]:
+            return func(file_path=file_path, parent=parent)
+
+        setattr(module, "pytest_collect_file", pytest_7_hook)
+        _logger.info(
+            "Installed pytest 7+ pytest_collect_file() hook into module `{}`".format(
+                module.__name__
+            )
+        )
+    else:
+        # pytest 6: expect a `py.path.local` path
+        def pytest_6_hook(
+            path: Any, parent: "pytest.Collector"
+        ) -> Optional[PytestFile]:
+            # Convert to `pathlib.Path`
+            return func(file_path=pathlib.Path(str(path)), parent=parent)
+
+        setattr(module, "pytest_collect_file", pytest_6_hook)
+        _logger.info(
+            "Installed pytest 6 pytest_collect_file() hook into module `{}`".format(
+                module.__name__
+            )
+        )
